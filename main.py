71524b31fc12c4bc0023957f6853a9a00c0f04ac
Cameron Hutchison <cameron.hutchison@vizientinc.com>
2023-06-26 13:35:44 -0700

    Format response as HTML and display it

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from langchain import PromptTemplate, SQLDatabase, SQLDatabaseChain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain 
 from langchain import LLMChain, PromptTemplate
+from IPython.display import display, HTML
 
 import os
-os.environ["OPENAI_API_KEY"] = ""
+os.environ["OPENAI_API_KEY"] = "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"
 
 # _DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct SQL query to run, then look at the results of the query and return the answer.
 # Use the following format:
@@ -25,6 +26,7 @@ os.environ["OPENAI_API_KEY"] = ""
 db = SQLDatabase.from_uri("sqlite:///./Chinook_Sqlite.sqlite")
 
 llm = ChatOpenAI(
+    model="gpt-3.5-turbo-16k",
     temperature=0,
     verbose=True
 )
@@ -38,6 +40,8 @@ _PROMPT_TEMPLATE = """You are an expert in communication. Your job is to inform 
 
 Given the following response to a user's question, create a new response to the same question that is more informative.
 
+Provide a structured response. The new response should be in an HTML format. Use tables where appropriate.
+
 Question: {question}
 Response: {response}
 
@@ -47,19 +51,25 @@ PROMPT = PromptTemplate(
     input_variables=["question", "response"],
     template=_PROMPT_TEMPLATE,
 )
-explain_chain = LLMChain(llm=llm, prompt=PROMPT)
+explain_chain = LLMChain(
+    llm=ChatOpenAI(
+        model="gpt-3.5-turbo-16k",
+        temperature=0.7
+    ),
+    prompt=PROMPT
+)
 
 
 # Final function
 def user_typed_prompt(question):
-    print("Question", question)
+    #print("Question", question)
     response = db_chain.run(question)
-    print("Response", question)
-    final_result = explain_chain.run(
+    #print("Response", response)
+    final_result_html = explain_chain.run(
         question=question,
         response=response,
     )
-    print("Final Result", final_result)
-    return final_result
+    # print("Final Result", final_result_html)
+    return display(HTML(final_result_html))
 
     
